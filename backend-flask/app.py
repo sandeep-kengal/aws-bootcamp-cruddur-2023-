d0ea0dc7244243d78f2bd9ddc2dd9cece7fb7a10
sandeep-kengal reddy <casandeepkengal@gmail.com>
2024-01-12 11:36:42 +0000

    update openapi to add notifications

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -13,6 +13,7 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+app = Flask(__name__)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
